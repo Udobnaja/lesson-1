64fa6f6f6dbc9a275caf516be86c9ed9ee522acb
udobnaja <udobnaja@gmail.com>
2018-03-08 23:35:03 +0300

    area for controls

--- src/index.js
+++ src/index.js
@@ -12,13 +12,13 @@ if ('content' in document.createElement('template')) {
     let container = template.content.querySelector('.feed__item').cloneNode(true);
     container.classList.add(FEED_TYPES[item.size]);
 
-
+    let description = container.querySelector('.feed__description');
     let title = container.querySelector('.feed__title');
+    let img = container.querySelector('.feed__img');
+    let chanel = container.querySelector('.feed__chanel');
 
     title.textContent = item.title;
     title.style.color = item.titleColor;
-
-    let description = container.querySelector('.feed__description');
 
     if (item.description){
       if (description){
@@ -31,7 +31,6 @@ if ('content' in document.createElement('template')) {
       description.remove();
     }
 
-    let img = container.querySelector('picture');
 
     if (item.image){
       let [imageName, imageExt] = item.image.split('.');
@@ -46,6 +45,12 @@ if ('content' in document.createElement('template')) {
       img.innerHTML = picture;
     } else {
       img.remove();
+    }
+
+    if (item.channelName){
+      chanel.textContent = item.channelName;
+    } else {
+      chanel.remove();
     }
 
     let clone = document.importNode(container, true);
